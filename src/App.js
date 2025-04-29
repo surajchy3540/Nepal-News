56985e862564c33c31a18c2221337df07fb326cc
Suraj Chaudhary <surajchy35407532@gmail.com>
2025-04-30 00:17:43 +0530

    hidding API key by adding custom Environment variable

--- src/App.js
+++ src/App.js
@@ -7,18 +7,19 @@ import LoadingBar from "react-top-loading-bar";
 
 export default class App extends Component {
   pageSize = 9;
+  apiKey = process.env.REACT_APP_NEWS_API;
   state = {
-    progress : 0
-  }
-  setProgress=(progress)=>{
-    this.setState({progress:progress})
-  }
+    progress: 0,
+  };
+  setProgress = (progress) => {
+    this.setState({ progress: progress });
+  };
   render() {
     return (
       <div>
         <Router>
           <LoadingBar
-          height={4}
+            height={4}
             color="#f11946"
             progress={this.state.progress}
             // onLoaderFinished={() => setProgress(0)}
@@ -30,7 +31,9 @@ export default class App extends Component {
               exact
               path="/"
               element={
-                <News setProgress={this.setProgress} 
+                <News
+                  setProgress={this.setProgress}
+                  apiKey={this.apiKey}
                   key="general"
                   pageSize={this.pageSize}
                   country="us"
@@ -42,7 +45,9 @@ export default class App extends Component {
               exact
               path="/business"
               element={
-                <News setProgress={this.setProgress} 
+                <News
+                  setProgress={this.setProgress}
+                  apiKey={this.apiKey}
                   key="business"
                   pageSize={this.pageSize}
                   country="us"
@@ -54,7 +59,9 @@ export default class App extends Component {
               exact
               path="/entertainment"
               element={
-                <News setProgress={this.setProgress} 
+                <News
+                  setProgress={this.setProgress}
+                  apiKey={this.apiKey}
                   key="entertainment"
                   pageSize={this.pageSize}
                   country="us"
@@ -66,7 +73,9 @@ export default class App extends Component {
               exact
               path="/general"
               element={
-                <News setProgress={this.setProgress} 
+                <News
+                  setProgress={this.setProgress}
+                  apiKey={this.apiKey}
                   key="general"
                   pageSize={this.pageSize}
                   country="us"
@@ -78,7 +87,9 @@ export default class App extends Component {
               exact
               path="/health"
               element={
-                <News setProgress={this.setProgress} 
+                <News
+                  setProgress={this.setProgress}
+                  apiKey={this.apiKey}
                   key="health"
                   pageSize={this.pageSize}
                   country="us"
@@ -90,7 +101,9 @@ export default class App extends Component {
               exact
               path="/science"
               element={
-                <News setProgress={this.setProgress} 
+                <News
+                  setProgress={this.setProgress}
+                  apiKey={this.apiKey}
                   key="science"
                   pageSize={this.pageSize}
                   country="us"
@@ -102,7 +115,9 @@ export default class App extends Component {
               exact
               path="/sports"
               element={
-                <News setProgress={this.setProgress} 
+                <News
+                  setProgress={this.setProgress}
+                  apiKey={this.apiKey}
                   key="sports"
                   pageSize={this.pageSize}
                   country="us"
@@ -114,7 +129,9 @@ export default class App extends Component {
               exact
               path="/technology"
               element={
-                <News setProgress={this.setProgress} 
+                <News
+                  setProgress={this.setProgress}
+                  apiKey={this.apiKey}
                   key="technology"
                   pageSize={this.pageSize}
                   country="us"
@@ -122,13 +139,13 @@ export default class App extends Component {
                 />
               }
             />
-            {/* <Route path="/business"> <News setProgress={this.setProgress}  pageSize={this.pageSize} country="us" category="business" /> </Route>
-            <Route path="/entertainment"> <News setProgress={this.setProgress}  pageSize={this.pageSize} country="us" category="entertainment" /> </Route>
-            <Route path="/general"> <News setProgress={this.setProgress}  pageSize={this.pageSize} country="us" category="general" /> </Route>
-            <Route path="/health"> <News setProgress={this.setProgress}  pageSize={this.pageSize} country="us" category="health" /> </Route>
-            <Route path="/science"> <News setProgress={this.setProgress}  pageSize={this.pageSize} country="us" category="science" /> </Route>
-            <Route path="/sports"> <News setProgress={this.setProgress}  pageSize={this.pageSize} country="us" category="sports" /> </Route>
-            <Route path="/technology"> <News setProgress={this.setProgress}  pageSize={this.pageSize} country="us" category="technology" /> </Route> */}
+            {/* <Route path="/business"> <News setProgress={this.setProgress} apiKey={this.apiKey}  pageSize={this.pageSize} country="us" category="business" /> </Route>
+            <Route path="/entertainment"> <News setProgress={this.setProgress} apiKey={this.apiKey}  pageSize={this.pageSize} country="us" category="entertainment" /> </Route>
+            <Route path="/general"> <News setProgress={this.setProgress} apiKey={this.apiKey}  pageSize={this.pageSize} country="us" category="general" /> </Route>
+            <Route path="/health"> <News setProgress={this.setProgress} apiKey={this.apiKey}  pageSize={this.pageSize} country="us" category="health" /> </Route>
+            <Route path="/science"> <News setProgress={this.setProgress} apiKey={this.apiKey}  pageSize={this.pageSize} country="us" category="science" /> </Route>
+            <Route path="/sports"> <News setProgress={this.setProgress} apiKey={this.apiKey}  pageSize={this.pageSize} country="us" category="sports" /> </Route>
+            <Route path="/technology"> <News setProgress={this.setProgress} apiKey={this.apiKey}  pageSize={this.pageSize} country="us" category="technology" /> </Route> */}
           </Routes>
         </Router>
       </div>
